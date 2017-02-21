f7fa13adf524197a52d20f874059c9218b7f9a9d
geniewoo <psw101004@korea.com>
2017-02-21 16:45:21 +0900

    making widget almost done

--- WidgetGame/app/src/main/java/com/sungwoo/boostcamp/widgetgame/widget/TxTWidget.java
+++ WidgetGame/app/src/main/java/com/sungwoo/boostcamp/widgetgame/widget/TxTWidget.java
@@ -6,18 +6,15 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
 
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
 import com.sungwoo.boostcamp.widgetgame.CommonUtility.ImageUtility;
+import com.sungwoo.boostcamp.widgetgame.LoginActivity;
 import com.sungwoo.boostcamp.widgetgame.R;
 import com.sungwoo.boostcamp.widgetgame.Repositories.FullGameRepo;
-import com.sungwoo.boostcamp.widgetgame.Repositories.GameInfo;
 import com.sungwoo.boostcamp.widgetgame.Repositories.Page;
 import com.sungwoo.boostcamp.widgetgame.Repositories.PlayGameRepo;
 import com.sungwoo.boostcamp.widgetgame.Repositories.Selection;
@@ -46,32 +43,29 @@ public class TxTWidget extends AppWidgetProvider {
     private static final String TAG = TxTWidget.class.getSimpleName();
     private static Realm mRealm = null;
     private static FullGameRepo mFullGameRepo = null;
-    private static final int CHANGE_CODE_PLAY_LAYOUT = 1;
-    private static final int CHANGE_CODE_NO_GAME = CHANGE_CODE_PLAY_LAYOUT + 1;
     private static final String ACTION_WIDGET_FLIPPER_BTN_CLICKED =
             "com.sungwoo.boostcamp.widgetgame.action.FLIPPER_LEFT_BTN_CLICKED";
-    private static final String ACTION_WIDGET_SELECT1_BTN_CLICKED =
-            "com.sungwoo.boostcamp.widgetgame.action.SELECT1_BTN_CLICKED";
-    private static final String ACTION_WIDGET_SELECT2_BTN_CLICKED =
-            "com.sungwoo.boostcamp.widgetgame.action.SELECT2_BTN_CLICKED";
-    private static final String ACTION_WIDGET_SELECT3_BTN_CLICKED =
-            "com.sungwoo.boostcamp.widgetgame.action.SELECT3_BTN_CLICKED";
-    private static final String ACTION_WIDGET_SELECT4_BTN_CLICKED =
-            "com.sungwoo.boostcamp.widgetgame.action.SELECT4_BTN_CLICKED";
     public static final String ACTION_WIDGET_DISPLAY_NEW_GAME =
             "com.sungwoo.boostcamp.widgetgame.action.DISPLAY_NEW_GAME";
-    public static final String ACTION_WIDGET_GAME_INFO_START_GAME_BTN =
+    private static final String ACTION_WIDGET_GAME_INFO_START_GAME_BTN =
             "com.sungwoo.boostcamp.widgetgame.action.GAME_INFO_START_BTN";
-    public static final String ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN =
+    private static final String ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN =
             "com.sungwoo.boostcamp.widgetgame.action.GAME_PLAYING_SELECTION_BTN";
-
-    public static final String PAGE_CHOICE = "Choice";
-    public static final String PAGE_STORY = "Story";
-    public static final String PAGE_GAME_OVER = "Game Over";
-    public static final String PAGE_GAME_CLEAR = "Game Clear";
+    private static final String ACTION_WIDGET_GAME_PLAYING_NO_SELECTION_NEXT_BTN =
+            "com.sungwoo.boostcamp.widgetgame.action.GAME_PLAYING_NO_SELECTION_NEXT_BTN";
+    private static final String ACTION_WIDGET_MENU_START_GAME_BTN =
+            "com.sungwoo.boostcamp.widgetgame.action.MENU_START_GAME_BTN";
+    public static final String ACTION_WIDGET_NO_GAME_START_APP_BTN =
+            "com.sungwoo.boostcamp.widgetgame.action.NO_GAME_START_APP_BTN";
+    public static final String ACTION_WIDGET_MENU_START_APP_BTN =
+            "com.sungwoo.boostcamp.widgetgame.action.MENU_START_APP_BTN";
+    private static final String PAGE_CHOICE = "Choice";
+    private static final String PAGE_STORY = "Story";
+    private static final String PAGE_GAME_OVER = "Game Over";
+    private static final String PAGE_GAME_CLEAR = "Game Clear";
 
     private static final int[] SELECTION_IDS = {R.id.widget_playing_game_selection1, R.id.widget_playing_game_selection2, R.id.widget_playing_game_selection3, R.id.widget_playing_game_selection4};
-
+    private static int[] mSelectedTargetIndex = new int[4];
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
@@ -91,6 +85,7 @@ public class TxTWidget extends AppWidgetProvider {
 
         switch (action) {
             case ACTION_WIDGET_DISPLAY_NEW_GAME:
+                setFullGameRepoMemberField();
                 showGameInfoLayout(context);
                 break;
             case ACTION_WIDGET_FLIPPER_BTN_CLICKED:
@@ -98,6 +93,35 @@ public class TxTWidget extends AppWidgetProvider {
                 break;
             case ACTION_WIDGET_GAME_INFO_START_GAME_BTN:
                 showGamePageLayout(context, 1);
+                break;
+            case ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN + "0":
+                showGamePageLayout(context, mSelectedTargetIndex[0]);
+                break;
+            case ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN + "1":
+                showGamePageLayout(context, mSelectedTargetIndex[1]);
+                break;
+            case ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN + "2":
+                showGamePageLayout(context, mSelectedTargetIndex[2]);
+                break;
+            case ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN + "3":
+                showGamePageLayout(context, mSelectedTargetIndex[3]);
+                break;
+            case ACTION_WIDGET_GAME_PLAYING_NO_SELECTION_NEXT_BTN:
+                switch (mNowStage) {
+                    case IN_GAME_CLEAR:
+                        flip(context);
+                        break;
+                    case IN_GAME_OVER:
+                        flip(context);
+                        break;
+                    case IN_STORY:
+                        showGamePageLayout(context, getPlayGameIndexPreference(context) + 1);
+                        break;
+                }
+                break;
+            case ACTION_WIDGET_MENU_START_GAME_BTN:
+                flip(context);
+                showGameInfoLayout(context);
                 break;
         }
     }
@@ -113,51 +137,56 @@ public class TxTWidget extends AppWidgetProvider {
             mAppWidgetIds.add(i);
             Log.d(TAG, "update : " + i);
         }
-
-        if (mFullGameRepo == null && mRealm.where(PlayGameRepo.class).findAll().size() == 1
+        mNowStage = IN_NO_GAME;
+        setPendingIntents(context);
+        if (mRealm.where(PlayGameRepo.class).findAll().size() == 1
                 && mRealm.where(PlayGameRepo.class).findAll().get(0).getPlayable()) {
-            Log.d(TAG, "setFullGameRepoMemberField");
-            setFullGameRepoMemberField();
-            int index = getPlayGameIndex(context);
-            mNowStage = IN_NO_GAME;
+            if (mFullGameRepo == null) {
+                setFullGameRepoMemberField();
+            }
+            int index = getPlayGameIndexPreference(context);
             if (index == 0) {
                 showGameInfoLayout(context);
             } else {
-                changeToPlayLayout(context);
                 showGamePageLayout(context, index);
             }
+        } else {
         }
-
     }
-
-    private int getPlayGameIndex(Context context) {
+    private void setPendingIntents(Context context) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
+        remoteViews.setOnClickPendingIntent(R.id.widget_game_info_start_game_btn,
+                getTxTWidgetPendingIntent(context, ACTION_WIDGET_GAME_INFO_START_GAME_BTN));
+        remoteViews.setOnClickPendingIntent(R.id.widget_flipper_btn,
+                getTxTWidgetPendingIntent(context, ACTION_WIDGET_FLIPPER_BTN_CLICKED));
+        remoteViews.setOnClickPendingIntent(R.id.widget_playing_game_no_selections_next_btn,  getTxTWidgetPendingIntent(context, ACTION_WIDGET_GAME_PLAYING_NO_SELECTION_NEXT_BTN));
+        remoteViews.setOnClickPendingIntent(R.id.widget_menu_start_game, getTxTWidgetPendingIntent(context, ACTION_WIDGET_MENU_START_GAME_BTN));
+        remoteViews.setOnClickPendingIntent(R.id.widget_menu_start_app, getTxTAppPendingIntent(context, ACTION_WIDGET_MENU_START_APP_BTN));
+        remoteViews.setOnClickPendingIntent(R.id.widget_no_game_start_app, getTxTAppPendingIntent(context, ACTION_WIDGET_NO_GAME_START_APP_BTN));
+        for (int i = 0 ; i < 4 ; i++)
+            remoteViews.setOnClickPendingIntent(SELECTION_IDS[i], getTxTWidgetPendingIntent(context, ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN + i));
+        AppWidgetManager.getInstance(context).updateAppWidget(intListToIntArray(mAppWidgetIds),
+                remoteViews);
+    }
+    private int getPlayGameIndexPreference(Context context) {
         SharedPreferences preferences = context.getSharedPreferences(
                 context.getString(R.string.PREF_PLAY_GAME), Context.MODE_PRIVATE);
         return preferences.getInt(context.getString(R.string.PREF_PLAY_GAME_INDEX), 0);
     }
 
-    private void changeLayout(Context context, int changeCode) {
-        switch (changeCode) {
-            case CHANGE_CODE_PLAY_LAYOUT:
-                changeToPlayLayout(context);
-                break;
-            case CHANGE_CODE_NO_GAME:
-                changeToNoGameLayout(context);
-                break;
-        }
+    private void setPlayGameIndexPreference(Context context, int index) {
+        SharedPreferences.Editor editor= context.getSharedPreferences(
+                context.getString(R.string.PREF_PLAY_GAME), Context.MODE_PRIVATE).edit();
+        editor.putInt(context.getString(R.string.PREF_PLAY_GAME_INDEX), index);
+        editor.apply();
     }
 
     private void changeToPlayLayout(Context context) {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
         remoteViews.setViewVisibility(R.id.widget_no_game_lo, View.GONE);
         remoteViews.setViewVisibility(R.id.widget_play_lo, View.VISIBLE);
-        remoteViews.setOnClickPendingIntent(R.id.widget_flipper_btn,
-                getTxTWidgetPendingIntent(context, ACTION_WIDGET_FLIPPER_BTN_CLICKED));
         AppWidgetManager.getInstance(context).updateAppWidget(intListToIntArray(mAppWidgetIds),
                 remoteViews);
-    }
-
-    private void setFlipperBtn(Context context, RemoteViews remoteViews) {
     }
 
     private void changeToNoGameLayout(Context context) {
@@ -178,6 +207,7 @@ public class TxTWidget extends AppWidgetProvider {
         }
         if (mAppWidgetIds.size() == 0 && mRealm != null) {
             mRealm.close();
+            mRealm = null;
         }
     }
 
@@ -201,11 +231,15 @@ public class TxTWidget extends AppWidgetProvider {
     }
 
     private void showGameInfoLayout(Context context) {
+        setPlayGameIndexPreference(context, 0);
+
         switch (mNowStage) {
             case IN_NO_GAME:
-                changeLayout(context, CHANGE_CODE_PLAY_LAYOUT);
+                changeToPlayLayout(context);
                 mNowStage = IN_INFO;
                 break;
+            default:
+                mNowStage = IN_INFO;
         }
 
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
@@ -225,8 +259,6 @@ public class TxTWidget extends AppWidgetProvider {
         remoteViews.setTextViewText(R.id.widget_game_info_email_tv, email);
         remoteViews.setTextViewText(R.id.widget_image_tv, "");
 
-        remoteViews.setOnClickPendingIntent(R.id.widget_game_info_start_game_btn,
-                getTxTWidgetPendingIntent(context, ACTION_WIDGET_GAME_INFO_START_GAME_BTN));
 
         AppWidgetManager.getInstance(context).updateAppWidget(intListToIntArray(mAppWidgetIds),
                 remoteViews);
@@ -245,12 +277,14 @@ public class TxTWidget extends AppWidgetProvider {
     }
 
     private void showGamePageLayout(Context context, int index) {
+        setPlayGameIndexPreference(context, index);
+
         switch (mNowStage) {
             case IN_NO_GAME:
-                changeLayout(context, CHANGE_CODE_PLAY_LAYOUT);
+                changeToPlayLayout(context);
                 break;
         }
-        Page page = mFullGameRepo.getGameInfo().getPages().get(index);
+        Page page = mFullGameRepo.getGameInfo().getPages().get(index - 1);
         switch (page.getPage()) {
             case PAGE_CHOICE:
                 mNowStage = IN_SELECTIONS;
@@ -269,6 +303,11 @@ public class TxTWidget extends AppWidgetProvider {
                 showGameStoryPageLayout(context, page);
                 break;
         }
+    }
+    private PendingIntent getTxTAppPendingIntent(Context context, String action) {
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.setAction(action);
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     private PendingIntent getTxTWidgetPendingIntent(Context context, String action) {
@@ -296,7 +335,7 @@ public class TxTWidget extends AppWidgetProvider {
 
         for (int i = 0 ; i < selections.size() ; i++) {
             remoteViews.setTextViewText(SELECTION_IDS[i], selections.get(i).getSelectionText());
-            remoteViews.setOnClickPendingIntent(SELECTION_IDS[i], getTxTWidgetPendingIntent(context, ACTION_WIDGET_GAME_PLAYING_SELECTION_BTN + i));
+            mSelectedTargetIndex[i] = selections.get(i).getNextIndex();
         }
 
         if (imagePath != context.getString(R.string.LOCAL_NO_IMAGE_FILE)) {
